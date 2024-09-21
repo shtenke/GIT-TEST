08694db4f5cf080c912765bda965adae394b9908
shtenke <shtenke.ps@mail.ru>
2024-09-21 14:57:45 +0300

    Купи Арканы

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом.")
+    bot.reply_to(message, "Привет! Я бот")
 
-@bot.message_handler(commands=['ban'])
+@bot.message_handler(commands=['kick'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
         chat_id = message.chat.id # сохранение id чата
@@ -16,10 +16,10 @@ def ban_user(message):
         user_status = bot.get_chat_member(chat_id, user_id).status 
          # проверка пользователя
         if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно забанить администратора.")
+            bot.reply_to(message, "Невозможно забанить админа.")
         else:
             bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
-            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} отлетел в таверну.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
